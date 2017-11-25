b131d60ac98c16524dd7ceaa61fa47363669a822
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-11-25 10:49:07 +0000

    PEP8 Python classes

--- optimaint/classes/station.py
+++ optimaint/classes/station.py
@@ -1,13 +1,14 @@
 class Station(object):
 
-  """
-  id: string
-  name: string
-  company: string (XC/WC)
-  train_limit: int
-  """
-  def __init__(self, id, name, company, train_limit):
-    self.id = id
-    self.name = name
-    self.company = company
-    self.train_limit = train_limit
+    """
+    id: string
+    name: string
+    company: string (XC/WC)
+    train_limit: int
+    """
+
+    def __init__(self, id, name, company, train_limit):
+        self.id = id
+        self.name = name
+        self.company = company
+        self.train_limit = train_limit
