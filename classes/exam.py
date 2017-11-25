cc03dffc0c70a4814149dd082ee38a222285a4d1
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-11-25 09:24:37 +0000

    Exam class

---
+++ classes/exam.py
@@ -0,0 +1,15 @@
+class Exam(object):
+  
+  """
+  type: string (C1/C2...)
+  mile_range: double
+  day_range: int
+  mile_tol: double
+  day_tol: int
+  """
+  def __init__(self, type, mile_range, day_range, mile_tol, day_tol):
+    self.type = type
+    self.mile_range = mile_range
+    self.day_range = day_range
+    self.mile_tol = mile_tol
+    self.day_tol = day_tol
