133ef3d8b6631c4647d8343a4dbf65ddeefff463
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-11-26 07:23:40 +0000

    Timetable setup

--- react_app/src/containers/timetable.js
+++ react_app/src/containers/timetable.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Title from '../components/title';
+import _ from 'lodash';
+import TimetableItem from '../components/timetable_item';
 
 class Timetable extends Component {
 
+  renderColumn(data) {
+    return data.map((d) => {
+      return (
+        <td key={d.trainID}><TimetableItem service={d} /></td>
+      );
+    });
+  }
+
+  renderRows(data) {
+    return data.map((d) => {
+      return (
+        <tr>{d}</tr>
+      );
+    });
+  }
+
+
   render() {
-    console.log(this.props.timetable);
+    var data = this.props.timetable;
+    for(var i=0;i<data.length;i++) {
+      while(data[i].length != 7) {
+        data[i].push({});
+      }
+    }
+    const data1 = data.map((d) => this.renderColumn(d));
+    const tbl = this.renderRows(data1);
+
     return (
       <div className="container">
         <Title />
         <table className="timetable">
           <thead className="timetable-header">
             <tr>
-              <th>Trains</th>
               <th>Monday</th>
               <th>Tuesday</th>
               <th>Wednesday</th>
@@ -23,6 +49,7 @@ class Timetable extends Component {
             </tr>
           </thead>
           <tbody className="timetable-body">
+            {tbl}
           </tbody>
         </table>
       </div>
--- react_app/src/reducers/reducer_timetable.js
+++ react_app/src/reducers/reducer_timetable.js
@@ -1,6 +1,23 @@
 export default function() {
   return [
-    {title: "Title 1"},
-    {title: "Title 2"}
+    [{ trainID: 222000, date: '25/11/2017', exam: 'C1', color:  '#FFFF00' },
+      { trainID: 222001, date: '25/11/2017', exam: 'S1', color: '#00E676' },
+      { trainID: 222002, date: '25/11/2017', exam: 'C2', color: '#FFFF00' },
+      { trainID: 222003, date: '25/11/2017', exam: 'S2', color: '#00E676' },
+      { trainID: 222402, date: '25/11/2017', exam: 'C2', color: '#FFFF00' },
+      { trainID: 222202, date: '25/11/2017', exam: 'C2', color: '#FFFF00' },
+      { trainID: 222302, date: '25/11/2017', exam: 'C2', color: '#FFFF00' },],
+    [{ trainID: 222006, date: '26/11/2017', exam: 'S1', color: '#00E676' },
+      { trainID: 222007, date: '26/11/2017', exam: 'MV1', color: '#FF3D00' },
+      { trainID: 222008, date: '26/11/2017', exam: 'S5', color: '#00E676' }],
+    [{ trainID: 222009, date: '27/11/2017', exam: 'C1', color: '#FFFF00' },
+      { trainID: 222010, date: '27/11/2017', exam: 'S1', color: '#00E676' }],
+    [{ trainID: 222011, date: '28/11/2017', exam: 'C2', color: '#FFFF00' },
+      { trainID: 222012, date: '28/11/2017', exam: 'MV2', color: '#FF3D00' },
+      { trainID: 222013, date: '28/11/2017', exam: 'S8', color: '#00E676' }],
+    [{ trainID: 222014, date: '29/11/2017', exam: 'C1', color: '#FFFF00' },
+      { trainID: 222015, date: '29/11/2017', exam: 'C2', color: '#FFFF00' },
+      { trainID: 222016, date: '29/11/2017', exam: 'S1', color: '#00E676' },
+      { trainID: 222017, date: '29/11/2017', exam: 'S2', color: '#00E676' }]
   ]
 }
